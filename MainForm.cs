f5668e8cc78d35c1b9722b04e2cb341484aa1b1e
kqye <154298198+kqye@users.noreply.github.com>
2024-05-05 20:46:30 +0800

    MainForm.cs

--- MainForm.cs
+++ MainForm.cs
@@ -45,3 +45,64 @@ namespace LoginSignUp
         }
     }
 }
+//FORM 2 CODE
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.DataFormats;
+
+namespace LoginSignUp
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            this.Hide();
+            new Form1().Show();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txtbUser.Text == "" || txtbLast.Text == "" || txtbUserIdw.Text == "")
+            {
+                MessageBox.Show("Incomplete Sign Up");
+            }
+            else if (txtbPass.Text == txtbConfirm.Text)
+            {
+                MessageBox.Show("Welcome " + txtbUserIdw.Text);
+                this.Hide();
+                //new Form3().Show();
+            }
+            else if (txtbPass.Text != txtbConfirm.Text)
+            {
+                MessageBox.Show("Passwords do not match");
+                txtbPass.Text = "";
+                txtbConfirm.Text = "";
+            }
+
+        }
+    }
+}
+
