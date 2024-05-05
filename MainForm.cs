89bdc5ac86f8a2d4704041f6df9edab822d41aec
kqye <154298198+kqye@users.noreply.github.com>
2024-05-05 20:45:26 +0800

    MainForm.cs

--- MainForm.cs
+++ MainForm.cs
@@ -1,88 +1,47 @@
-﻿/*
- * Created by SharpDevelop.
- * User: acer
- * Date: 15/04/2024
- * Time: 7:06 pm
- * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
- */
-using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Windows.Forms;
-
-namespace windowsform
+namespace LoginSignUp
 {
-	/// <summary>
-	/// Description of MainForm.
-	/// </summary>
-	public partial class MainForm : Form
-	{
-		public MainForm()
-		{
-			//
-			// The InitializeComponent() call is required for Windows Forms designer support.
-			//
-			InitializeComponent();
-			
-			//
-			// TODO: Add constructor code after the InitializeComponent() call.
-			//
-		}
-		
-		void Label1Click(object sender, EventArgs e)
-		{
-			
-		}
-		
-		void MainFormLoad(object sender, EventArgs e)
-		{
-			
-		}
-		
-		void CheckBox1CheckedChanged(object sender, EventArgs e)
-		{
-			string login; 
-			string user = "boset";
-			string pass; 
-			string password = "dino";
-			
-			
-				login = Convert.ToString(textBox1.Text);
-				pass = Convert.ToString(textBox2.Text);
-				
-				if(login==user&&pass==password)
-				{
-					MessageBox.Show("Login Successfully");
-				}
-				else
-				{
-					MessageBox.Show("Login Failed"); 
-				}
-				//if(login!=user||pass!=password)
-				//{
-				//	MessageBox.Show("Invalid Username");
-				//}
-				//else
-				//{
-				//	MessageBox.Show("Login Failed");
-				//}
-				//if(login==user&&pass==password)
-				//{
-				//	MessageBox.Show("Invalid Password");
-				//}
-				//else
-				//{
-				//	MessageBox.Show("Login Failed");
-				//}
-				//if(login==user&&pass==password)
-				//MessageBox.Show("Invalid Account");
-			
-		}
-		
-		void TextBox1TextChanged(object sender, EventArgs e)
-		{
-			
-		}
-	}
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string login;
+            string user = "username";
+            string pass;
+            string password = "pass";
+
+
+            login = Convert.ToString(textBox1.Text);
+            pass = Convert.ToString(textBox2.Text);
+
+            if (login == user && pass == password)
+            {
+                MessageBox.Show("Login Successfully");
+            }
+            else
+            {
+                MessageBox.Show("Wrong Username or Password");
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            this.Hide();
+            new Form2().Show();
+        }
+    }
 }
